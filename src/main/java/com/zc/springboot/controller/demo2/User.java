7a06da616f15f468054278cc8112f2d154349d9c
djzc <djzc@zcDePc>
2018-12-26 10:32:56 +0800

    代码提交

--- src/main/java/com/zc/springboot/controller/demo2/User.java
+++ src/main/java/com/zc/springboot/controller/demo2/User.java
@@ -1,9 +1,18 @@
 package com.zc.springboot.controller.demo2;
 
-public class User {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private String name;
 	private String age;
+	private String regTime;
+	private String email;
 
 	public String getName() {
 		return name;
@@ -19,6 +28,22 @@ public class User {
 
 	public void setAge(String age) {
 		this.age = age;
+	}
+
+	public String getRegTime() {
+		return regTime;
+	}
+
+	public void setRegTime(String regTime) {
+		this.regTime = regTime;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
