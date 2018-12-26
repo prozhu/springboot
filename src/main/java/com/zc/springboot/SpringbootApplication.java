7ee9b1ce5f1d8e962bcfb364f062d23e667491ed
zhucheng <zhucheng@192.168.13.173>
2018-12-26 10:31:21 +0800

    修改信息

--- src/main/java/com/zc/springboot/SpringbootApplication.java
+++ src/main/java/com/zc/springboot/SpringbootApplication.java
@@ -2,8 +2,14 @@ package com.zc.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+/*
+ * springboot 启动类，只会扫描，这个文件所在包的子包，因此需要加上指定的扫描注解，让其扫描指定包底下的接口
+ */
+
+@ComponentScan(basePackages = {"com.zc.springboot"})
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
