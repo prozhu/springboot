c4f8c4a731f214f643534445392283359727b912
djzc <djzc@zcDePc>
2019-01-03 13:38:56 +0800

    将钉钉机器人的webhook 转移到配置文件中（可配置灵活些）

--- src/main/java/com/zc/springboot/config/DingDingConstantConfig.java
+++ src/main/java/com/zc/springboot/config/DingDingConstantConfig.java
@@ -1,11 +1,28 @@
 package com.zc.springboot.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
 /**
  * 钉钉机器人webhook 地址配置
  * @author ：djzc
  * @createTime ：2019年1月2日 下午4:02:13 
  * @updateTime ：2019年1月2日 下午4:02:13
  */
+@Component
+@PropertySource({ "classpath:dingDingWebhookConfig.properties" })
 public class DingDingConstantConfig {
-	public static final String CHATBOT_WEBHOOK = "https://oapi.dingtalk.com/robot/send?access_token=915fd85b16b7d3662887f8f3a1150bb47446c497171117225a6e1461d44be147";
+
+	@Value("${java.study.webhook}")
+	private String charbotWebhook;
+
+	public String getCharbotWebhook() {
+		return charbotWebhook;
+	}
+
+	public void setCharbotWebhook(String charbotWebhook) {
+		this.charbotWebhook = charbotWebhook;
+	}
+
 }
--- src/main/java/com/zc/springboot/controller/demo1/DingDingController.java
+++ src/main/java/com/zc/springboot/controller/demo1/DingDingController.java
@@ -3,6 +3,7 @@ package com.zc.springboot.controller.demo1;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,12 @@ import com.zc.springboot.config.DingDingConstantConfig;
 @RestController
 public class DingDingController {
 
+	/**
+	 * 从配置文件中读取机器人的webhook
+	 */
+	@Autowired
+	private DingDingConstantConfig config;
+
 	@RequestMapping(method = RequestMethod.GET, value = { "testDingDing" })
 	public SendResult testDingDing(String name, String age) throws IOException {
 		DingtalkChatbotClient client = new DingtalkChatbotClient();
@@ -28,8 +35,8 @@ public class DingDingController {
 		ArrayList<String> atMobiles = new ArrayList<String>();
 		atMobiles.add("15927294078");
 		message.setAtMobiles(atMobiles);
-		message.setIsAtAll(true);
-		SendResult result = client.send(DingDingConstantConfig.CHATBOT_WEBHOOK, message);
+//		message.setIsAtAll(true);
+		SendResult result = client.send(config.getCharbotWebhook(), message);
 		System.out.println(result);
 		return result;
 	}
