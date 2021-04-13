fbc79625feaf354af1c22ac47970d287a68b806d
zc <1>
2021-04-13 10:22:30 +0800

    add: 新增校验注解：@PatternFilterSpecChar

--- src/main/java/com/open/utils/validator/constraints/PatternExcludeNextline.java
+++ src/main/java/com/open/utils/validator/constraints/PatternExcludeNextline.java
@@ -14,6 +14,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 /**
  * 校验注解
  * （排除换行符之后，在进行校验，也就是把“换行符替换为空格")
+ * ps： 使用示例请参考：
+ * @see TestPatternExincludeNextline
  * @author zc
  */
 @Target({FIELD})
