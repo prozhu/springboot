a27a9013723828db241021725ee44372993a85a9
zhucheng <1>
2022-07-29 10:20:06 +0800

    mod:工具类中，新增获取ip地址的方法

--- src/main/java/com/open/utils/http/HttpUtils.java
+++ src/main/java/com/open/utils/http/HttpUtils.java
@@ -8,6 +8,9 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
@@ -74,5 +77,45 @@ public class HttpUtils {
      */
     public static String sendPostRequest(String url) {
         return sendPostRequest(url, null);
+    }
+
+    /**
+     * 获取请求的ip地址
+     * @author zhucheng
+     * @date 2022/7/29 10:12
+     * @param request
+     * @return java.lang.String
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String unknown = "unknown";
+        String ipAddress = request.getHeader("x-forwarded-for");
+        if (ipAddress == null || ipAddress.length() == 0 || unknown.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || unknown.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || unknown.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+            String localHost = "127.0.0.1";
+            String mac = "0:0:0:0:0:0:0:1";
+            if (localHost.equals(ipAddress) || mac.equals(ipAddress)) {
+                //根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ipAddress = inet.getHostAddress();
+            }
+        }
+        //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ipAddress != null && ipAddress.length() > 15) {
+            if (ipAddress.indexOf(",") > 0) {
+                ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+            }
+        }
+        return ipAddress;
     }
 }
